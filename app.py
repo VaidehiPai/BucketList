68ecb1536551990d5f111341896e1145b35bb4cc
VaidehiPai <166487197+VaidehiPai@users.noreply.github.com>
2024-11-23 22:28:26 +0530

    Update app.py
    Removed some unnecessary codes

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, jsonify
 from pymongo import MongoClient
 
-client=MongoClient('mongodb+srv://test:sparta@cluster0.ia0nj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+client=MongoClient('mongodb+srv://test:@cluster0.ia0nj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
 db=client.dbsparta
 app = Flask(__name__)
 
